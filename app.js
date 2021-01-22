ba3a14d8065f7535aaa23919f6e4af0567f9310a
mowie <sam@mowrer.dev>
2021-01-22 09:13:50 -0600

    Have node serving up html/css files. Working on js file.

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ var app = express();
 var path = require("path");
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 // use res.render to load up an ejs view file
-app.use( express.static(__dirname + "/pages" ) );
-app.use( express.static(__dirname + "/css" ) );
-app.use( express.static(__dirname + "/javascript" ) );
+// app.use( express.static(__dirname + "/public" ) );
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 // index page
 app.get('/', function(req, res) {
     res.render('index', {});
 });
-
-
 
 app.listen(3001, function () {
   console.log('App listening on port 3001!');
